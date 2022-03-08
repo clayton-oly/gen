8455c52efcfa4449af07778faa201c38cfdf046d
Clayton <clayton.will08@gmail.com>
2022-03-08 17:06:34 -0300

    Subindo exercicios de Polimorfismo e Heranca

---
+++ Polimorfismo e herança/src/br/com/generation/polimorfismo/Preguica.java
@@ -0,0 +1,13 @@
+package br.com.generation.polimorfismo;
+
+public class Preguica extends Animal {
+
+	public void emitirSom() {
+		System.out.println("Emitindo som do preguica");
+	}
+	
+	public void subirArvores() {
+		System.out.println("Preguica esta subindo na arvore");
+	}
+
+}
